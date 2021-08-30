0ec74efc3b12c8a41e0e27b50210b63e61b2ab4d
Uladzimir Basak <Uladzimir.Basak@softeq.com>
2021-08-30 09:51:56 +0300

    lesson1: ex1, ex2

---
+++ lesson-1/ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            string currentDate = DateTime.UtcNow.ToString("MM dddd yyyy");
+            Console.WriteLine($"Hello {userName}, today is {currentDate}");
+        }
+    }
+}
