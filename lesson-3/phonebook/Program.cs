966048a012a3667193a93df62b0b4b615b29aeb1
Uladzimir Basak <Uladzimir.Basak@softeq.com>
2021-09-06 12:05:15 +0300

    lesson-3

---
+++ lesson-3/phonebook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] phonebook = new string[5,2] {{"Mark Flores","MarkFlores@armyspy.com"},{"Mary Lee","MaryLee@dayrep.com" },{"Christina Colon","ChristinaDColon@rhyta.com" },{ "Sophie Klug", "SophieKlug@jourrapide.com" },{ "Ming Li", "MingLi@rhyta.com" }} ;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"{phonebook[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
